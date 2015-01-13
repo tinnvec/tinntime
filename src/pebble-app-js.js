fe1366233ed1b54db7d9d9a138f8c2ef61cae03b
Alex Hanna <tinnvec@gmail.com>
2015-01-12 20:00:25 -0600

    Revert "Even more code cleanup"
    This reverts commit d9172b30f156adf9e6c94741d11febe37b952bfc.

--- src/pebble-app-js.js
+++ src/pebble-app-js.js
@@ -5,19 +5,21 @@ function capitalizeEachWord(str) {
 }
 
 function locationSuccess(pos) {
+  // http://api.openweathermap.org/data/2.5/weather?q=53913,USA
+  // console.log('lat= ' + pos.coords.latitude + ' lon= ' + pos.coords.longitude);
   var configuration, url;
   var req = new XMLHttpRequest();
-
+  
   if (window.localStorage.getItem("tinntime_config") !== null) {
     configuration = JSON.parse(window.localStorage.tinntime_config);
   }
-
+  
   if (configuration.gps === "no") {
     url = "http://api.openweathermap.org/data/2.5/weather?q=" + configuration.zipcode + ",USA";
   } else {
     url = "http://api.openweathermap.org/data/2.5/weather?lat=" + pos.coords.latitude + "&lon=" + pos.coords.longitude;
   }
-
+  
   req.open('GET', url, true);
   req.onload = function(e) {
     if (req.readyState == 4 && req.status == 200) {
@@ -28,25 +30,30 @@ function locationSuccess(pos) {
         var icon = response.weather[0].icon;
         var location = response.name;
         var conditions = capitalizeEachWord(response.weather[0].description);
-
+        
         if (window.localStorage.getItem("tinntime_config") !== null) {
           configuration = JSON.parse(window.localStorage.tinntime_config);
         }
-
+        
         // Temperature in Kelvin requires adjustment
         if(configuration.temp == "c") {
           temperature = Math.round(temperature - 273.15);
         } else {
           temperature = Math.round(temperature * 9/5 - 459.675);
         }
-
+        
+        console.log("Temperature is " + temperature);
+        console.log("Conditions Icon: " + icon);
+        console.log("Location is " + location);
+        console.log("Conditions are " + conditions);
+        
         var dictionary = {
           "KEY_TEMPERATURE": temperature,
           "KEY_CONDITIONS_ICON": icon,
           "KEY_CONDITIONS": conditions,
           "KEY_LOCATION": location
         };
-
+        
         Pebble.sendAppMessage(dictionary, function(e) {
           console.log("Weather info sent to Pebble successfully!");
         }, function(e) {
@@ -64,17 +71,17 @@ function locationError(err) {
 
 function getWeather() {
   var configuration;
-
+  
   if (window.localStorage.getItem("tinntime_config") !== null) {
     configuration = JSON.parse(window.localStorage.tinntime_config);
   }
-
+  
   if (configuration.gps === "no") {
     locationSuccess(null);
   } else {
     var locationOptions = {
-      enableHighAccuracy: true,
-      maximumAge: 10000,
+      enableHighAccuracy: true, 
+      maximumAge: 10000, 
       timeout: 10000
     };
     navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locationOptions);
@@ -85,10 +92,10 @@ function getWeather() {
 Pebble.addEventListener('ready', function(e) {
   if (window.localStorage.getItem("tinntime_config") === null) {
     var configuration = {
-      "invert": "no",
+      "invert": "yes",
       "temp": "f",
       "gps": "yes",
-      "zipcode": "00000",
+      "zipcode": "000000",
       "update": "30"
     };
     window.localStorage.tinntime_config = JSON.stringify(configuration);
@@ -125,12 +132,12 @@ Pebble.addEventListener('webviewclosed', function(e) {
   var configuration = JSON.parse(decodeURIComponent(e.response));
   window.localStorage.tinntime_config = JSON.stringify(configuration);
   console.log("Configuration window returned: " + JSON.stringify(configuration));
-
+  
   var dictionary = {
     "KEY_INVERT": configuration.invert,
     "KEY_UPDATE": configuration.update
   };
-
+  
   //Send to Pebble, persist there
   Pebble.sendAppMessage(dictionary, function(e) {
     console.log("Settings data sent successfully!");
